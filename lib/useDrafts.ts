9f0896de24463d2a38461b0b8a8528dbe1f769b5
meaghanfitzgerald <meaghan.e.fitzgerald@ey.com>
2022-10-11 00:38:08 -0400

    feature: created drafts page and included routing

--- lib/useDrafts.ts
+++ lib/useDrafts.ts
@@ -5,7 +5,6 @@ import { Proposal } from "@prisma/client";
 export function useDrafts() {
   const { data: drafts, isLoading } = useQuery(["drafts"], async () => {
     const response = await axios.get<Proposal[]>("/api/drafts");
-    console.log('🚀 ~ file: useDraftProposals.ts ~ response', response);
     return response.data;
   });
   return { drafts, isLoading };
